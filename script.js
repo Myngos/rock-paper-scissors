b6843ff15b14be76dd9d40013ac42e7f300b6046
Domingo Monius <[moniusdomingo065@gmail.com]>
2024-08-25 18:57:53 +0200

    correct getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello Mingos');
+function getComputerChoice(){
+
+    const choices = ["rock", "paper", "scissors"];//choices the computer can make
+
+    const randomChoice = Math.floor(Math.random()*choices.length);
+
+    return choices[randomChoice];
+}
+
+console.log(getComputerChoice());
